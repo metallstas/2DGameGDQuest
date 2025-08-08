7c758e4392e9a6a0a0abf7d8f736f7093da9fa13
Стас Хмылко <metallstas@mail.ru>
2025-08-08 19:17:58 +0300

    add bullet

--- Guns/gun.gd
+++ Guns/gun.gd
@@ -1,1 +1,7 @@
 extends Area2D
+
+func _physics_process(delta: float) -> void:
+	var enemies_in_range = get_overlapping_bodies()
+	if enemies_in_range.size() > 0:
+		var target_enemie = enemies_in_range[0]
+		look_at(target_enemie.global_position)
