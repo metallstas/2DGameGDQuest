898d351fef7a28adf3f690f7c1952c4868b065b4
Стас Хмылко <metallstas@mail.ru>
2025-08-09 11:15:37 +0300

    added dmg enemies, dmg player

--- Guns/gun.gd
+++ Guns/gun.gd
@@ -5,3 +5,13 @@ func _physics_process(delta: float) -> void:
 	if enemies_in_range.size() > 0:
 		var target_enemie = enemies_in_range[0]
 		look_at(target_enemie.global_position)
+
+func shoot():
+	const BULLET = preload("res://Guns/bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+
+func _on_timer_timeout() -> void:
+	shoot()
--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
+signal health_depleted
+
 const SPEED := 600.0
+var health := 100.0
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -13,3 +16,11 @@ func _physics_process(delta: float) -> void:
 		player.play_walk_animation()
 	else:
 		player.play_idle_animation()
+	
+	const DAMAGE_RATE = 5.0
+	var overlapping_enemies = %HurtBox.get_overlapping_bodies()
+	
+	if overlapping_enemies.size() > 0:
+		health -= DAMAGE_RATE * overlapping_enemies.size() * delta
+		if health <= 0.0:
+			health_depleted.emit()
